e3624e7dc6541f46bfa67d1a84c666f161471531
nicholasSUSE <nicholas.paolillo@suse.com>
2025-02-17 14:50:45 -0300

    udpate run.sh

--- aws/aws-registry.sh
+++ aws/aws-registry.sh
@@ -2,31 +2,28 @@
 
 set -x
 
+INSTANCE_DNS=$(terraform output -raw workload_1_dns)
 
-# Declare INSTANCE_DNS as a global variable
-INSTANCE_DNS=""
-
-function tf() {
+function destroy() {
 	terraform destroy --auto-approve || true
 	rm -Rf .terraform || true
 	rm .terraform.lock.hcl || true
 	rm terraform.tfstate* || true
+}
+
+function init() {
   terraform init -reconfigure
   terraform apply --auto-approve
-
-  # Get the instance DNS from Terraform output
-  INSTANCE_DNS=$(terraform output -raw workload_1_dns)
-
-  if [ -z "$INSTANCE_DNS" ]; then
-    echo "Failed to retrieve instance DNS from Terraform output"
+  # Check if the instance is reachable using ping
+  if !ping -c 1 "$INSTANCE_DNS" >/dev/null 2>&1; then
+    echo "Failed to ping instance: $INSTANCE_DNS"
     exit 1
+  else
+    echo "Successfully pinged instance: $INSTANCE_DNS"
   fi
-
-  echo "Instance DNS: $INSTANCE_DNS"
 }
 
 function tls_docker() {
-
   sudo rm /etc/docker/daemon.json
   sudo touch /etc/docker/daemon.json
   echo '{"insecure-registries": ["'"$INSTANCE_DNS"'"]}' | sudo tee /etc/docker/daemon.json
@@ -45,37 +42,8 @@ function tls_docker() {
 }
 
 
-function helper() {
-  # Disable command logging
-  set +x
-
-  echo -e "________________________________________________________\n"
-  echo -e "TLS Docker registry setup complete\n"
-
-  echo -e "INSTANCE_DNS=$INSTANCE_DNS\n"
-
-  echo -e "Next steps: \n"
-  echo "1. Log in to Docker"
-  echo -e "docker login \$INSTANCE_DNS \n"
-
-  echo "2. List repositories"
-  echo -e "curl -X GET https://\$INSTANCE_DNS/v2/_catalog --cacert /etc/docker/certs.d/\$INSTANCE_DNS/ca.crt --insecure \n"
-
-  echo "3. List tags (versions)"
-  echo "curl -X GET https://\$INSTANCE_DNS/v2/<repository-name>/tags/list --cacert /etc/docker/certs.d/\$INSTANCE_DNS/ca.crt --insecure \n"
-
-  echo "4. Push an image"
-  echo -e "helm push ../assets/rancher-istio/rancher-istio-104.2.0+up1.20.3.tgz oci://\$INSTANCE_DNS --ca-file /etc/docker/certs.d/\$INSTANCE_DNS/ca.crt --insecure-skip-tls-verify \n"
-
-  echo "5. Pull an image"
-  echo -e "helm pull oci://\$INSTANCE_DNS/rancher-istio:104.2.0+up1.20.3 --ca-file /etc/docker/certs.d/\$INSTANCE_DNS/ca.crt --insecure-skip-tls-verify --debug \n"
-
-  # Re-enable command logging
-  set -x
-}
 
 
-
-tf
+destroy
+init
 tls_docker
-helper
--- run.sh
+++ run.sh
@@ -1,18 +1,57 @@
 #!/bin/bash
 
 set -x
-
-work_dir=$(pwd)
+# Global Variables
+INSTANCE_DNS=""
+WORK_DIR=$(pwd)
+OCI_DIR="$WORK_DIR/oci-test"
+AWS_DIR="$WORK_DIR/aws"
 
 # check if it is empty and clone the repo
 if [ -z "$(ls -A oci-test)" ]; then
     git clone git@github.com:nicholasSUSE/oci-test.git
 else
     echo "The folder 'oci-test' is not empty."
-    cd oci-test
+    cd $OCI_DIR
     git fetch
     git pull
 fi
 
-cd $work_dir
-ls
+cd $WORK_DIR
+ls -lah $OCI_DIR
+
+cd $AWS_DIR
+./aws-registry.sh || { echo "./aws-registry.sh failed"; exit 1; } # Crucial change!
+
+# Get the instance DNS from Terraform output
+INSTANCE_DNS=$(terraform output -raw workload_1_dns)
+cd $WORK_DIR
+
+
+
+# Disable command logging
+set +x
+
+echo -e "________________________________________________________\n"
+echo -e "TLS Docker registry setup complete\n"
+
+echo -e "INSTANCE_DNS=$INSTANCE_DNS\n"
+
+echo -e "Next steps: \n"
+echo "1. Log in to Docker"
+echo -e "docker login \$INSTANCE_DNS \n"
+
+echo "2. List repositories"
+echo -e "curl -X GET https://\$INSTANCE_DNS/v2/_catalog --cacert /etc/docker/certs.d/\$INSTANCE_DNS/ca.crt --insecure \n"
+
+echo "3. List tags (versions)"
+echo "curl -X GET https://\$INSTANCE_DNS/v2/<repository-name>/tags/list --cacert /etc/docker/certs.d/\$INSTANCE_DNS/ca.crt --insecure \n"
+
+echo "4. Push an image"
+echo -e "helm push ../assets/rancher-istio/rancher-istio-104.2.0+up1.20.3.tgz oci://\$INSTANCE_DNS --ca-file /etc/docker/certs.d/\$INSTANCE_DNS/ca.crt --insecure-skip-tls-verify \n"
+
+echo "5. Pull an image"
+echo -e "helm pull oci://\$INSTANCE_DNS/rancher-istio:104.2.0+up1.20.3 --ca-file /etc/docker/certs.d/\$INSTANCE_DNS/ca.crt --insecure-skip-tls-verify --debug \n"
+
+# Re-enable command logging
+set -x
